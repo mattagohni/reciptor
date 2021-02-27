56833ae6e1b7789f644a427fd82bbcb9a9df0d2e
Matthias Alt <mattagohni@gmail.com>
2021-02-27 16:49:45 +0100

    add ui-lib for with basic header-component

---
+++ libs/shared/ui-header/src/lib/header/header.component.ts
@@ -0,0 +1,11 @@
+import {ChangeDetectionStrategy, Component, ViewEncapsulation} from '@angular/core';
+
+@Component({
+  selector: 'reciptor-header',
+  templateUrl: './header.component.html',
+  styleUrls: ['./header.component.scss'],
+  encapsulation: ViewEncapsulation.Emulated,
+  changeDetection: ChangeDetectionStrategy.OnPush
+})
+export class HeaderComponent {
+}

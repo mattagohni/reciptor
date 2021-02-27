5cc19e42a3f09a4c0cf50643b42af9121ffe143a
Matthias Alt <mattagohni@gmail.com>
2021-02-27 17:10:48 +0100

    fix imports in tests

--- libs/shared/ui-header/src/lib/header/header.component.spec.ts
+++ libs/shared/ui-header/src/lib/header/header.component.spec.ts
@@ -1,6 +1,7 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { HeaderComponent } from './header.component';
+import {SharedMaterialModule} from '@reciptor/shared/material';
 
 describe('HeaderComponent', () => {
   let component: HeaderComponent;
@@ -8,7 +9,8 @@ describe('HeaderComponent', () => {
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      declarations: [ HeaderComponent ]
+      declarations: [ HeaderComponent ],
+      imports: [SharedMaterialModule]
     })
     .compileComponents();
   });

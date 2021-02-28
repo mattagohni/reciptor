f3232b6cdf7efab34bd52f859ac7c638035beb4a
Matthias Alt <mattagohni@gmail.com>
2021-02-28 09:54:09 +0100

    add ngx-translate with MultiHttpLoader to be able to load translations from libs

--- apps/reciptor/src/app/app.module.ts
+++ apps/reciptor/src/app/app.module.ts
@@ -5,14 +5,34 @@ import {AppComponent} from './app.component';
 import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
 import {SharedMaterialModule} from '@reciptor/shared/material';
 import {SharedUiHeaderModule} from '@reciptor/shared/ui-header';
+import {TranslateLoader, TranslateModule} from '@ngx-translate/core';
+import {HttpClient, HttpClientModule} from '@angular/common/http';
+import {MultiTranslateHttpLoader} from 'ngx-translate-multi-http-loader';
+
+// AoT requires an exported function for factories
+export function HttpLoaderFactory(http: HttpClient) {
+  return new MultiTranslateHttpLoader(http, [
+    {prefix: './assets/i18n/', suffix: '.json'},
+    {prefix: './assets/i18n/ui-header/', suffix: '.json'}
+  ]);
+}
 
 @NgModule({
   declarations: [AppComponent],
   imports: [
     BrowserModule,
     BrowserAnimationsModule,
-    SharedMaterialModule,
+    HttpClientModule,
     SharedUiHeaderModule,
+    SharedMaterialModule,
+    TranslateModule.forRoot({
+      defaultLanguage: 'de',
+      loader: {
+        provide: TranslateLoader,
+        useFactory: HttpLoaderFactory,
+        deps: [HttpClient]
+      }
+    })
   ],
   providers: [],
   bootstrap: [AppComponent],
--- libs/shared/ui-header/src/lib/shared-ui-header.module.ts
+++ libs/shared/ui-header/src/lib/shared-ui-header.module.ts
@@ -2,9 +2,10 @@ import {NgModule} from '@angular/core';
 import {CommonModule} from '@angular/common';
 import {HeaderComponent} from './header/header.component';
 import {SharedMaterialModule} from '@reciptor/shared/material';
+import {TranslateModule} from '@ngx-translate/core';
 
 @NgModule({
-  imports: [CommonModule, SharedMaterialModule],
+  imports: [CommonModule, SharedMaterialModule, TranslateModule.forChild()],
   declarations: [HeaderComponent],
   exports: [HeaderComponent],
 })

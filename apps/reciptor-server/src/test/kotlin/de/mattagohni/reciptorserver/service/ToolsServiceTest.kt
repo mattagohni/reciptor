3fffec16bb719f57cbda03c0615cd3517f7ad2f3
Matthias Alt <mattagohni@gmail.com>
2021-03-04 16:52:17 +0100

    feat(reciptor-server): replace mockito-occurences with mockk

--- apps/reciptor-server/src/test/kotlin/de/mattagohni/reciptorserver/service/ToolsServiceTest.kt
+++ apps/reciptor-server/src/test/kotlin/de/mattagohni/reciptorserver/service/ToolsServiceTest.kt
@@ -1,17 +1,16 @@
 package de.mattagohni.reciptorserver.service
 
+import com.ninjasquad.springmockk.MockkBean
 import de.mattagohni.reciptor.model.Tool
 import de.mattagohni.reciptorserver.repository.ReactiveToolsRepository
+import io.mockk.every
+import io.mockk.verify
 import org.assertj.core.api.Assertions.assertThat
 import org.junit.jupiter.api.DisplayName
 import org.junit.jupiter.api.Test
 import org.junit.jupiter.api.assertThrows
-import org.mockito.Mockito.`when`
-import org.mockito.Mockito.times
-import org.mockito.Mockito.verify
 import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.boot.test.context.SpringBootTest
-import org.springframework.boot.test.mock.mockito.MockBean
 import reactor.core.publisher.Mono
 import reactor.test.StepVerifier
 
@@ -21,7 +20,7 @@ class ToolsServiceTest {
   @Autowired
   private lateinit var toolsService: ToolsService
 
-  @MockBean
+  @MockkBean
   private lateinit var toolsRepository: ReactiveToolsRepository
 
   @Test
@@ -29,13 +28,13 @@ class ToolsServiceTest {
   fun canSaveATool() {
     // arrange
     val tool = Tool(id = "SomeId", name = "theMasterTool")
-    `when`(toolsRepository.save(tool)).thenReturn(Mono.just(tool))
+    every { toolsRepository.save(tool) } returns Mono.just(tool)
 
     // act
     val result = toolsService.saveTool(tool)
 
     // assert
-    verify(toolsRepository, times(1)).save(tool)
+    verify { toolsRepository.save(tool) }
     StepVerifier.create(result)
       .assertNext { resultingTool -> assertThat(resultingTool.name).isEqualTo("theMasterTool") }
       .verifyComplete()
@@ -46,13 +45,13 @@ class ToolsServiceTest {
   fun throwWhenSaveNotPossible() {
     // arrange
     val tool = Tool(id = "SomeId", name = "theMasterTool")
-    `when`(toolsRepository.save(tool)).thenThrow(RuntimeException("something went wrong"))
+    every { toolsRepository.save(tool) }.throws(RuntimeException("something went wrong"))
 
     // act
     val exception = assertThrows<RuntimeException> { toolsService.saveTool(tool) }
 
     // assert
-    verify(toolsRepository, times(1)).save(tool)
+    verify { toolsRepository.save(tool) }
     assertThat(exception.message).isEqualTo("something went wrong")
   }
 
@@ -61,7 +60,7 @@ class ToolsServiceTest {
   fun returnExisting() {
     // arrange
     val tool = Tool(id = "SomeId", name = "theMasterTool")
-    `when`(toolsRepository.findByName("theMasterTool")).thenReturn(Mono.just(tool))
+    every { toolsRepository.findByName(any<String>()) }.returns(Mono.just(tool))
 
     // act
     val resultingToolMono = toolsService.findToolByName("theMasterTool")
@@ -70,6 +69,6 @@ class ToolsServiceTest {
     StepVerifier.create(resultingToolMono)
       .assertNext { resultingTool -> assertThat(resultingTool).isEqualTo(tool) }
       .verifyComplete()
-    verify(toolsRepository, times(1)).findByName("theMasterTool")
+    verify { toolsRepository.findByName("theMasterTool") }
   }
 }

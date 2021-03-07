78a36213274b3270a6526c9ae45d929580d3a8f7
Matthias Alt <mattagohni@gmail.com>
2021-03-07 12:14:11 +0100

    feat(reciptor-server): add flyway db-migrations
    at the moment spring boot can not run migrations on startup using the r2dbc-driver. So we have to run migrations with the maven plugin for flyway

--- apps/reciptor-server/src/main/kotlin/de/mattagohni/reciptorserver/model/Tool.kt
+++ apps/reciptor-server/src/main/kotlin/de/mattagohni/reciptorserver/model/Tool.kt
@@ -1,9 +1,11 @@
 package de.mattagohni.reciptorserver.model
 
 import org.springframework.data.annotation.Id
+import org.springframework.data.relational.core.mapping.Table
 
+@Table
 data class Tool(
   @Id
-  val id: Int?,
+  val id: Long?,
   val name: String
 )
---
+++ apps/reciptor-server/src/main/resources/migrations/V1__create_tool_table.sql
@@ -0,0 +1,6 @@
+create table if not exists tool
+(
+    id  bigserial
+        primary key,
+    name varchar(255)
+);

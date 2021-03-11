b371256e4141d05c81f80f2840fec984b3f33979
Matthias Alt <mattagohni@gmail.com>
2021-03-11 14:02:37 +0100

    feat(reciptor): lazyload ToolsUiModule for route 'tools'

--- apps/reciptor/src/app/app-routing.module.ts
+++ apps/reciptor/src/app/app-routing.module.ts
@@ -3,6 +3,7 @@ import {CommonModule} from '@angular/common';
 import {RouterModule, Routes} from '@angular/router';
 
 const routes: Routes = [
+  {path: 'tools', loadChildren: () => import('@reciptor/tools/ui').then(module => module.ToolsUiModule)}
 ]
 
 @NgModule({

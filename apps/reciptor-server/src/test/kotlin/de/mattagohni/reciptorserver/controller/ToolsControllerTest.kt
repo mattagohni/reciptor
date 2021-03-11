cd7d3ff20a61be35c554f27b57073dc08e4a840b
Matthias Alt <mattagohni@gmail.com>
2021-03-11 11:50:46 +0100

    feat(reciptor-server): add controller method to get list of all tools

--- apps/reciptor-server/src/test/kotlin/de/mattagohni/reciptorserver/controller/ToolsControllerTest.kt
+++ apps/reciptor-server/src/test/kotlin/de/mattagohni/reciptorserver/controller/ToolsControllerTest.kt
@@ -18,6 +18,7 @@ import org.springframework.http.HttpStatus
 import org.springframework.security.test.context.support.WithMockUser
 import org.springframework.test.web.reactive.server.WebTestClient
 import org.springframework.web.reactive.function.BodyInserters
+import reactor.core.publisher.Flux
 import reactor.core.publisher.Mono
 
 // suppressed because mockk produces a false-positive when returning a mono
@@ -33,19 +34,56 @@ class ToolsControllerTest {
   private lateinit var toolsService: ToolsService
 
   @Test
+  @DisplayName("it returns a list of all tools with status OK")
+  @WithMockUser
+  fun getTools() {
+    val toolsFlux = Flux.just(
+      Tool(id = 1, name = "knife"),
+      Tool(id = 2, name = "spoon")
+    )
+
+    every { toolsService.getAll() } returns toolsFlux
+
+    webTestClient.get().uri("/api/v1/tools")
+      .exchange()
+      .expectStatus().isOk
+      .expectBody()
+      .jsonPath("$.[0].name").isEqualTo("knife")
+      .jsonPath("$.[1].name").isEqualTo("spoon")
+
+    verify(exactly = 1) { toolsService.getAll() }
+  }
+
+  @Test
+  @DisplayName("it returns empty list with status OK when no tools exist")
+  @WithMockUser
+  fun getTools_emptyList() {
+    val toolsFlux = Flux.empty<Tool>()
+
+    every { toolsService.getAll() } returns toolsFlux
+
+    webTestClient.get().uri("/api/v1/tools")
+      .exchange()
+      .expectStatus().isOk
+      .expectBody().json("[]")
+
+    verify(exactly = 1) { toolsService.getAll() }
+  }
+
+  @Test
   @DisplayName("it returns a responseEntity containing a tool")
   @WithMockUser
   fun getTool() {
     // arrange
     val tool = Tool(id = 1, name = "knife")
-    every { toolsService.findToolById(any<Int>()) } returns Mono.just(tool)
+    every { toolsService.findToolById(any()) } returns Mono.just(tool)
 
     webTestClient.get().uri("/api/v1/tools/1")
       .exchange()
       .expectStatus().isOk
       .expectBody().jsonPath("$.name").isEqualTo("knife")
 
-    verify { toolsService.findToolById(1) }
+    verify(exactly = 1) { toolsService.findToolById(1) }
   }
 
   @Test
@@ -59,7 +97,7 @@ class ToolsControllerTest {
       .exchange()
       .expectStatus().isNotFound
 
-    verify { toolsService.findToolById(1) }
+    verify(exactly = 1) { toolsService.findToolById(1) }
   }
 
   @Test
@@ -70,7 +108,7 @@ class ToolsControllerTest {
     val toolBeforeSave = Tool(id = null, name = "knife")
     val toolAfterSave = Tool(id = 1, name = "knife")
 
-    every { toolsService.saveTool(any<Tool>()) }.returns(Mono.just(toolAfterSave))
+    every { toolsService.saveTool(any()) }.returns(Mono.just(toolAfterSave))
 
     // act
     webTestClient.post().uri("/api/v1/tools")
@@ -91,7 +129,7 @@ class ToolsControllerTest {
     // arrange
     val toolToSave = Tool(id = null, name = "knife")
 
-    every { toolsService.saveTool(any<Tool>()) }.throws(ToolAlreadyExistsException(HttpStatus.CONFLICT))
+    every { toolsService.saveTool(any()) }.throws(ToolAlreadyExistsException(HttpStatus.CONFLICT))
 
     // act
     webTestClient.post().uri("/api/v1/tools")
@@ -99,6 +137,6 @@ class ToolsControllerTest {
       .exchange()
       .expectStatus().isEqualTo(HttpStatus.CONFLICT)
 
-    verify(exactly = 1) { toolsService.saveTool(any<Tool>()) }
+    verify(exactly = 1) { toolsService.saveTool(any()) }
   }
 }

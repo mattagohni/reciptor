a9167e560a779593689b7bee35e0437c8bc2537c
Matthias Alt <mattagohni@gmail.com>
2021-03-12 13:30:05 +0100

    feat(tools-ui): use facade of tools-data-access to retrieve tools from store

--- libs/tools/ui/src/lib/tool-list/tool-list.component.ts
+++ libs/tools/ui/src/lib/tool-list/tool-list.component.ts
@@ -1,10 +1,19 @@
-import {Component} from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import {ToolsEntity, ToolsFacade} from '@reciptor/tools/data-access';
+import {Observable} from 'rxjs';
 
 @Component({
   selector: 'reciptor-tool-list',
   templateUrl: './tool-list.component.html',
   styleUrls: ['./tool-list.component.scss']
 })
-export class ToolListComponent{
+export class ToolListComponent implements OnInit {
+  tools$: Observable<ToolsEntity[]>
+  constructor(private toolsFacade: ToolsFacade) {}
+
+  ngOnInit(): void {
+    this.toolsFacade.init()
+    this.tools$ = this.toolsFacade.allTools$
+  }
 }
 
--- libs/tools/ui/src/lib/tools-ui.module.ts
+++ libs/tools/ui/src/lib/tools-ui.module.ts
@@ -2,11 +2,15 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { ToolListComponent } from './tool-list/tool-list.component';
 import {RouterModule} from '@angular/router';
+import {ToolsDataAccessModule} from '@reciptor/tools/data-access';
+import {SharedMaterialModule} from '@reciptor/shared/material';
 
 @NgModule({
   imports: [
     CommonModule,
-    RouterModule.forChild([{path: '', component: ToolListComponent}])
+    RouterModule.forChild([{path: '', component: ToolListComponent}]),
+    ToolsDataAccessModule,
+    SharedMaterialModule
   ],
   declarations: [ToolListComponent],
 })

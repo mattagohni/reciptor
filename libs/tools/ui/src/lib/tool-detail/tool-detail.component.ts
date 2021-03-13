d341a0e1a48c605ecb71d8037114a93cd577e5b6
Matthias Alt <mattagohni@gmail.com>
2021-03-13 14:02:27 +0100

    feat(tools-ui): display all data of a tool in a mat-card

--- libs/tools/ui/src/lib/tool-detail/tool-detail.component.ts
+++ libs/tools/ui/src/lib/tool-detail/tool-detail.component.ts
@@ -1,4 +1,5 @@
-import {ChangeDetectionStrategy, Component} from '@angular/core';
+import {ChangeDetectionStrategy, Component, Input} from '@angular/core';
+import {Tool} from '@reciptor/tools/data-access';
 
 @Component({
   selector: 'reciptor-tool',
@@ -7,4 +8,6 @@ import {ChangeDetectionStrategy, Component} from '@angular/core';
   changeDetection: ChangeDetectionStrategy.OnPush
 })
 export class ToolDetailComponent {
+  @Input()
+  tool: Tool
 }

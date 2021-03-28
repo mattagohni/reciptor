8110d12f199a2b3a3e24cf2d5dcbc4719ed3aceb
Matthias Alt <mattagohni@gmail.com>
2021-03-28 11:50:39 +0200

    fix(tools): import ToolsUiModule into ToolsFeatureModule
    also import Forms and reactiveForms-Module in ToolsUiModule

--- libs/tools/feature/src/lib/tools-feature.module.ts
+++ libs/tools/feature/src/lib/tools-feature.module.ts
@@ -1,14 +1,14 @@
 import {NgModule} from '@angular/core';
 import {CommonModule} from '@angular/common';
 import {RouterModule} from '@angular/router';
-import {ToolDetailComponent} from '@reciptor/tools/ui';
-import {ToolListComponent} from '@reciptor/tools/ui';
+import {ToolDetailComponent, ToolListComponent, ToolsUiModule} from '@reciptor/tools/ui';
 import {ToolsDataAccessModule} from '@reciptor/tools/data-access';
 
 @NgModule({
   imports: [
     CommonModule,
     ToolsDataAccessModule,
+    ToolsUiModule,
     RouterModule.forChild([
       {path: '', component: ToolListComponent },
       {path: ':id', component: ToolDetailComponent}
--- libs/tools/ui/src/lib/tools-ui.module.ts
+++ libs/tools/ui/src/lib/tools-ui.module.ts
@@ -5,10 +5,13 @@ import {RouterModule} from '@angular/router';
 import {ToolsDataAccessModule} from '@reciptor/tools/data-access';
 import {SharedMaterialModule} from '@reciptor/shared/material';
 import { ToolDetailComponent } from './tool-detail/tool-detail.component';
+import {FormsModule, ReactiveFormsModule} from '@angular/forms';
 
 @NgModule({
   imports: [
     CommonModule,
+    FormsModule,
+    ReactiveFormsModule,
     RouterModule.forChild([{path: '', component: ToolListComponent}]),
     ToolsDataAccessModule,
     SharedMaterialModule

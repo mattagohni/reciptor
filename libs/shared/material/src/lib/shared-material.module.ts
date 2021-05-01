7d4faea209b7de2fd3a1fc20d4773de32f9d14e8
Matthias Alt <mattagohni@gmail.com>
2021-05-01 11:45:51 +0200

    feat(reciptor): add component to create new tools

--- libs/shared/material/src/lib/shared-material.module.ts
+++ libs/shared/material/src/lib/shared-material.module.ts
@@ -4,9 +4,10 @@ import { MatInputModule } from '@angular/material/input';
 import { MatCardModule } from '@angular/material/card';
 import { MatToolbarModule } from '@angular/material/toolbar';
 import {MatTableModule} from '@angular/material/table';
+import {MatMenuModule} from '@angular/material/menu';
 
 @NgModule({
-  imports: [FlexLayoutModule, MatInputModule, MatCardModule, MatTableModule, MatToolbarModule],
-  exports: [FlexLayoutModule, MatInputModule, MatCardModule, MatTableModule, MatToolbarModule],
+  imports: [FlexLayoutModule, MatInputModule, MatCardModule, MatTableModule, MatToolbarModule, MatMenuModule],
+  exports: [FlexLayoutModule, MatInputModule, MatCardModule, MatTableModule, MatToolbarModule, MatMenuModule],
 })
 export class SharedMaterialModule {}

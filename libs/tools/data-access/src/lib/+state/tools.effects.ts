9005749c6d27133af0656c2a9ee96d47b7aa4d0e
Matthias Alt <mattagohni@gmail.com>
2021-05-13 17:21:36 +0200

    build(nx): upgrade nx to v12 change implementation of tools.effects.ts after upgrade

--- libs/tools/data-access/src/lib/+state/tools.effects.ts
+++ libs/tools/data-access/src/lib/+state/tools.effects.ts
@@ -1,97 +1,62 @@
 import {Injectable} from '@angular/core';
 import {Actions, createEffect, ofType} from '@ngrx/effects';
-import {fetch} from '@nrwl/angular';
 import * as ToolsActions from './tools.actions';
 import {ToolsService} from '../tools.service';
-import {map} from 'rxjs/operators';
+import {map, mergeMap} from 'rxjs/operators';
 import {Tool} from '@reciptor/tools/data-access';
 
 @Injectable()
 export class ToolsEffects {
-  init$ = createEffect(() =>
-    this.actions$.pipe(
+  init$ = createEffect(
+    () => this.actions$.pipe(
       ofType(ToolsActions.init),
-      fetch({
-        // @todo remove this suppression when the variable is used
-        // eslint-disable-next-line @typescript-eslint/no-unused-vars
-        run: () => {
-          return this.toolsService.getAll()
-            .pipe(
-              map(tools => ToolsActions.loadToolsSuccess({tools}))
-            );
-        },
-
-        onError: (action, error) => {
-          console.error('Error', error);
-          return ToolsActions.loadToolsFailure({error});
-        },
-      })
-    )
-  );
+      mergeMap(() => {
+        return this.toolsService.getAll()
+          .pipe(
+            map(tools => ToolsActions.loadToolsSuccess({tools}))
+          );
+      }),
+    ));
 
   loadTool$ = createEffect(() =>
     this.actions$.pipe(
       ofType(ToolsActions.loadToolById),
-      fetch({
-        run: (action) => {
-          return this.toolsService.getTool(action.id).pipe(
-            map(tool => ToolsActions.loadToolSuccess({tool}))
-          )
-        },
-        onError: (action, error) => {
-          return ToolsActions.loadToolFailure({error});
-        }
+      mergeMap((action) => {
+        return this.toolsService.getTool(action.id).pipe(
+          map(tool => ToolsActions.loadToolSuccess({tool}))
+        )
       })
-    )
-  );
+    ));
 
   deleteTool$ = createEffect(() =>
     this.actions$.pipe(
       ofType(ToolsActions.deleteToolById),
-      fetch({
-        run: (action) => {
-          return this.toolsService.deleteTool(action.id).pipe(
-            map(() => ToolsActions.deleteToolByIdSuccess({id: action.id}))
-          )
-        },
-        onError: (action, error) => {
-          return ToolsActions.deleteToolByIdFailure({error})
-        }
+      mergeMap((action) => {
+        return this.toolsService.deleteTool(action.id).pipe(
+          map(() => ToolsActions.deleteToolByIdSuccess({id: action.id}))
+        )
       })
-    )
-  );
+    ));
 
   updateTool$ = createEffect(() =>
     this.actions$.pipe(
       ofType(ToolsActions.updateTool),
-      fetch({
-        run: (action) => {
-          return this.toolsService.updateTool(action.tool).pipe(
-            map((tool: Tool) => ToolsActions.updateToolSuccess({tool}))
-          )
-        },
-        onError: (action, error) => {
-          return ToolsActions.updateToolFailure({error});
-        }
+      mergeMap((action) => {
+        return this.toolsService.updateTool(action.tool).pipe(
+          map((tool: Tool) => ToolsActions.updateToolSuccess({tool}))
+        )
       })
-    )
-  );
+    ));
 
   saveTool$ = createEffect(() =>
     this.actions$.pipe(
       ofType(ToolsActions.saveTool),
-      fetch({
-        run: (action) => {
-          return this.toolsService.saveTool(action.tool).pipe(
-            map((tool: Tool) => ToolsActions.saveToolSuccess({tool}))
-          )
-        },
-        onError: (action, error) => {
-          return ToolsActions.saveToolFailure({error});
-        }
+      mergeMap((action) => {
+        return this.toolsService.saveTool(action.tool).pipe(
+          map((tool: Tool) => ToolsActions.saveToolSuccess({tool}))
+        )
       })
-    )
-  );
+    ));
 
   constructor(private actions$: Actions, private toolsService: ToolsService) {
   }

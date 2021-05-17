82e0eea1fd34ec702bfb0c7c6983493dcc828e7d
Matthias Alt <mattagohni@gmail.com>
2021-05-17 21:19:54 +0200

    feat(reciptor-server): add custom passwordEncoder

---
+++ apps/reciptor-server/src/main/kotlin/de/mattagohni/reciptorserver/service/PBKDF2Encoder.kt
@@ -0,0 +1,38 @@
+package de.mattagohni.reciptorserver.service
+
+import de.mattagohni.reciptorserver.configuration.PasswordEncoderConfiguration
+import org.springframework.security.crypto.password.PasswordEncoder
+import org.springframework.stereotype.Component
+import java.security.NoSuchAlgorithmException
+import java.security.spec.InvalidKeySpecException
+import java.util.Base64
+import javax.crypto.SecretKeyFactory
+import javax.crypto.spec.PBEKeySpec
+
+
+@Component
+class PBKDF2Encoder(private val configuration: PasswordEncoderConfiguration) : PasswordEncoder {
+  override fun encode(input: CharSequence?): String {
+    return try {
+      val result = SecretKeyFactory.getInstance("PBKDF2WithHmacSHA512")
+        .generateSecret(
+          PBEKeySpec(
+            input.toString().toCharArray(),
+            configuration.secret!!.encodeToByteArray(),
+            configuration.iteration!!,
+            configuration.keylength!!
+          )
+        )
+        .encoded
+      Base64.getEncoder().encodeToString(result)
+    } catch (ex: NoSuchAlgorithmException) {
+      throw RuntimeException(ex)
+    } catch (ex: InvalidKeySpecException) {
+      throw RuntimeException(ex)
+    }
+  }
+
+  override fun matches(input: CharSequence?, candidate: String?): Boolean {
+    return encode(input) == candidate
+  }
+}
---
+++ apps/reciptor-server/src/test/kotlin/de/mattagohni/reciptorserver/service/PBKDF2EncoderTest.kt
@@ -0,0 +1,54 @@
+package de.mattagohni.reciptorserver.service
+
+import org.assertj.core.api.Assertions.assertThat
+import org.junit.jupiter.api.DisplayName
+import org.junit.jupiter.api.Test
+import org.springframework.beans.factory.annotation.Autowired
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest(
+  properties = [
+    "reciptor.password.encoder.secret=superSecret",
+    "reciptor.password.encoder.iteration=25",
+    "reciptor.password.encoder.keylength=512",
+  ]
+)
+internal class PBKDF2EncoderTest {
+  @Autowired
+  private lateinit var pbkdF2Encoder: PBKDF2Encoder
+
+  @Test
+  @DisplayName("it encodes a given password")
+  fun encodePassword() {
+    // arrange
+    val stringToEncode = "mySuperSecretPassword"
+
+    // act
+    val result = pbkdF2Encoder.encode(stringToEncode)
+    // assert
+
+    assertThat(result).isEqualTo("PlVA9bq4fIUNZTlg2hRcIY8NPeXAEdD4+Xti2HLo8n8tIIhG5Lvz9xW3helgIfp4raz8PWN9GzGiPohZAogclg==")
+  }
+
+  @Test
+  @DisplayName("it matches correct password")
+  fun matchCorrectPassword() {
+    assertThat(
+      pbkdF2Encoder.matches(
+        "mySuperSecretPassword",
+        "PlVA9bq4fIUNZTlg2hRcIY8NPeXAEdD4+Xti2HLo8n8tIIhG5Lvz9xW3helgIfp4raz8PWN9GzGiPohZAogclg=="
+      )
+    ).isTrue
+  }
+
+  @Test
+  @DisplayName("it does not match for wrong password")
+  fun rejectIncorrectPassword() {
+    assertThat(
+      pbkdF2Encoder.matches(
+        "myUltraWrongPassword",
+        "PlVA9bq4fIUNZTlg2hRcIY8NPeXAEdD4+Xti2HLo8n8tIIhG5Lvz9xW3helgIfp4raz8PWN9GzGiPohZAogclg=="
+      )
+    ).isFalse
+  }
+}

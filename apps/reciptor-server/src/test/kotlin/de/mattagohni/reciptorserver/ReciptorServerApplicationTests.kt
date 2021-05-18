a13cde50dea44eb5289f1675d5a183d02547252f
Matthias Alt <mattagohni@gmail.com>
2021-05-18 16:03:36 +0200

    test(reciptor-server): set the reciptor.jwt.secret property in test to a value satisfying jjwt

--- apps/reciptor-server/src/test/kotlin/de/mattagohni/reciptorserver/ReciptorServerApplicationTests.kt
+++ apps/reciptor-server/src/test/kotlin/de/mattagohni/reciptorserver/ReciptorServerApplicationTests.kt
@@ -8,7 +8,7 @@ import org.springframework.boot.test.context.SpringBootTest
     "reciptor.password.encoder.secret=superSecret",
     "reciptor.password.encoder.iteration=25",
     "reciptor.password.encoder.keylength=512",
-    "reciptor.jwt.secret=alsoVerySecret",
+    "reciptor.jwt.secret=ThisIsSecretForJWTHS512SignatureAlgorithmThatMUSTHave64ByteLength",
     "reciptor.jwt.expiration=3600"
   ]
 )

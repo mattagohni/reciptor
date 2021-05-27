c4a85454b29533aad3fd7d2cf307152c7d563bd5
Matthias Alt <mattagohni@gmail.com>
2021-05-27 16:55:20 +0200

    feat(reciptor): add Observable for isLoggedIn
    in order to be able to determine a logged in user the authentication.service.ts now propagates an Observable with the current state of the user.

--- libs/authentication/data-access/src/lib/service/authentication.service.spec.ts
+++ libs/authentication/data-access/src/lib/service/authentication.service.spec.ts
@@ -5,16 +5,13 @@ import {HttpClientTestingModule, HttpTestingController} from '@angular/common/ht
 import {ReciptorAuthenticationRequest} from '../types/authentication.request';
 import {ReciptorAuthenticationResponse} from '../types/authentication.response';
 import {RECIPTOR_API_URL} from '@reciptor/app-config';
+import {cold} from '@nrwl/angular/testing'
+import * as moment from 'moment';
 
 describe('AuthenticationService', () => {
   let service: AuthenticationService;
   let httpClient: HttpTestingController;
-
   beforeEach(() => {
-    jest.spyOn(window.localStorage.__proto__, 'setItem');
-    jest.spyOn(window.localStorage.__proto__, 'removeItem');
-    window.localStorage.__proto__.setItem = jest.fn();
-    window.localStorage.__proto__.removeItem = jest.fn();
 
     TestBed.configureTestingModule({
       imports: [HttpClientTestingModule],
@@ -33,6 +30,10 @@ describe('AuthenticationService', () => {
 
     const authResponse: ReciptorAuthenticationResponse = {token: 'someToken', expires: expiresAt};
 
+    // verify the observable is initialized with false
+    expect(service.loggedIn$).toBeObservable(cold('a', {a:false}))
+
+    // do login
     service.login(authRequest).subscribe(
       response => {
         expect(response.token).toEqual('someToken');
@@ -46,16 +47,44 @@ describe('AuthenticationService', () => {
     expect(request.request.method).toEqual('POST');
     httpClient.verify();
 
-    expect(localStorage.setItem).toBeCalledTimes(2);
-    expect(localStorage.setItem).toHaveBeenNthCalledWith(1, 'id_token', 'someToken');
-    expect(localStorage.setItem).toHaveBeenNthCalledWith(2, 'expires_at', expiresAt.toString().valueOf());
+    expect(localStorage.getItem('id_token')).toEqual('someToken');
+    expect(localStorage.getItem('expires_at')).toEqual(expiresAt.toString().valueOf());
+    // verify the observable is set to the correct value after succesful login
+    expect(service.loggedIn$).toBeObservable(cold('b', {b:true}))
   });
 
   it('should clear localStorage on logout', () => {
+    localStorage.setItem('id_token', 'someToken');
+    localStorage.setItem('expires_at', moment(moment.now()).unix().toString())
+
     service.logout();
 
-    expect(localStorage.removeItem).toBeCalledTimes(2)
-    expect(localStorage.removeItem).toHaveBeenNthCalledWith(1, 'id_token');
-    expect(localStorage.removeItem).toHaveBeenNthCalledWith(2, 'expires_at');
+    expect(localStorage.getItem('id_token')).toBeNull();
+    expect(localStorage.getItem('expires_at')).toBeNull();
+  });
+
+  describe('isLoggedIn$', () => {
+
+    it('should have a value of false when initialized with no token in store', () => {
+      const expected = cold('a', {a: false});
+
+      expect(service.loggedIn$).toBeObservable(expected);
+    });
+
+    it('should have a value of false when initialized with an expired token', () => {
+      localStorage.setItem('id_token', 'someToken');
+      localStorage.setItem('expires_at', moment(moment.now() - 3600).unix().toString());
+      const expected = cold('a', {a: false});
+
+      expect(service.loggedIn$).toBeObservable(expected);
+    })
+
+    it('should have a value of true when initialized with a token which is not expired', () => {
+      localStorage.setItem('id_token', 'someToken');
+      localStorage.setItem('expires_at', moment(moment.now() + 3600).unix().toString());
+      const expected = cold('a', {a: true});
+
+      expect(service.loggedIn$).toBeObservable(expected);
+    })
   });
 });

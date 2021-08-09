b42a0863c8ae5dbf4a22dadf6fc8c6f304e10c66
Matthias Alt <matthias.alt@datev.de>
2021-08-09 10:33:28 +0200

    test(authentication): add test for behaviour, if no token is present in localStorage

--- libs/reciptor/authentication/data-access/src/lib/interceptor/auth-interceptor.interceptor.spec.ts
+++ libs/reciptor/authentication/data-access/src/lib/interceptor/auth-interceptor.interceptor.spec.ts
@@ -10,8 +10,6 @@ describe('AuthInterceptorInterceptor', () => {
   let httpMock: HttpTestingController;
 
   beforeEach(() => {
-    jest.spyOn(window.localStorage.__proto__, 'getItem');
-    window.localStorage.__proto__.getItem = jest.fn(() => 'someIdToken');
     TestBed.configureTestingModule({
       imports: [HttpClientTestingModule],
       providers: [
@@ -23,18 +21,40 @@ describe('AuthInterceptorInterceptor', () => {
     httpMock = TestBed.inject(HttpTestingController);
   });
 
-  it('should add Bearer-Token to every request', () => {
-    service.exampleAction().subscribe((value) => {
-      expect(value).not.toBeNull();
+  describe('token is present', () => {
+    beforeEach(() => {
+      jest.spyOn(window.localStorage.__proto__, 'getItem');
+      window.localStorage.__proto__.getItem = jest.fn(() => 'someIdToken');
+    })
+    it('should add Bearer-Token to every request', () => {
+      service.exampleAction().subscribe((value) => {
+        expect(value).not.toBeNull();
+      });
+
+      const httpRequest = httpMock.expectOne('http://example.com');
+
+      expect(httpRequest.request.headers.has('Authorization')).toBeTruthy();
+      expect(httpRequest.request.headers.get('Authorization')).toMatch(
+          /^Bearer /
+      );
     });
+  })
 
-    const httpRequest = httpMock.expectOne('http://example.com');
 
-    expect(httpRequest.request.headers.has('Authorization')).toBeTruthy();
-    expect(httpRequest.request.headers.get('Authorization')).toMatch(
-      /^Bearer /
-    );
-  });
+  describe('no token is present', () => {
+    beforeEach(() => {
+      jest.spyOn(window.localStorage.__proto__, 'getItem');
+      window.localStorage.__proto__.getItem = jest.fn(() => undefined);
+    })
+    it('should not add token', () => {
+      service.exampleAction().subscribe((value) => {
+        expect(value).not.toBeNull();
+      });
+
+      const httpRequest = httpMock.expectOne('http://example.com');
+      expect(httpRequest.request.headers.has('Authorization')).toBeFalsy();
+    });
+  })
 });
 
 @Injectable()

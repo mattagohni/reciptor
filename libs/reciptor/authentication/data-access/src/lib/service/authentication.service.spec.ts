06f0e0bb7ee2bb28ad6fd8e56d591adcd9093410
Matthias Alt <matthias.alt@datev.de>
2021-08-10 08:34:37 +0200

    refactor(reciptor-authentication-data-access): add describe-blocks for login- and logout-methods

--- libs/reciptor/authentication/data-access/src/lib/service/authentication.service.spec.ts
+++ libs/reciptor/authentication/data-access/src/lib/service/authentication.service.spec.ts
@@ -26,82 +26,86 @@ describe('AuthenticationService', () => {
     httpClient = TestBed.inject(HttpTestingController);
   });
 
-  it('should send a authentication request', (done) => {
-    const authRequest: ReciptorAuthenticationRequest = {
-      username: 'mattagohni',
-      password: 'myPassword',
-    };
-    const expiresAt = Date.now() + 1000;
+  describe('login', () => {
+    it('should send a authentication request', (done) => {
+      const authRequest: ReciptorAuthenticationRequest = {
+        username: 'mattagohni',
+        password: 'myPassword',
+      };
+      const expiresAt = Date.now() + 1000;
 
-    const authResponse: ReciptorAuthenticationResponse = {
-      token: 'someToken',
-      expires: expiresAt,
-    };
+      const authResponse: ReciptorAuthenticationResponse = {
+        token: 'someToken',
+        expires: expiresAt,
+      };
 
-    // verify the observable is initialized with false
-    expect(service.loggedIn$).toBeObservable(cold('a', {a: false}));
+      // verify the observable is initialized with false
+      expect(service.loggedIn$).toBeObservable(cold('a', {a: false}));
 
-    // do login
-    service.login(authRequest).subscribe((response) => {
-      expect(response.token).toEqual('someToken');
-      done();
-    });
+      // do login
+      service.login(authRequest).subscribe((response) => {
+        expect(response.token).toEqual('someToken');
+        done();
+      });
 
-    const request = httpClient.expectOne('http://reciptor.mattagohni.de/login');
+      const request = httpClient.expectOne('http://reciptor.mattagohni.de/login');
 
-    request.flush(authResponse);
-    expect(request.request.method).toEqual('POST');
-    httpClient.verify();
+      request.flush(authResponse);
+      expect(request.request.method).toEqual('POST');
+      httpClient.verify();
 
-    expect(localStorage.getItem('id_token')).toEqual('someToken');
-    expect(localStorage.getItem('expires_at')).toEqual(
-      expiresAt.toString().valueOf()
-    );
-    // verify the observable is set to the correct value after succesful login
-    expect(service.loggedIn$).toBeObservable(cold('b', {b: true}));
-  });
-
-  it('should clear localStorage on logout', () => {
-    localStorage.setItem('id_token', 'someToken');
-    localStorage.setItem('expires_at', moment(moment.now()).unix().toString());
-
-    service.logout();
-
-    expect(localStorage.getItem('id_token')).toBeNull();
-    expect(localStorage.getItem('expires_at')).toBeNull();
-  });
-
-  describe('isLoggedIn$', () => {
-    it('should have a value of false when initialized with no token in store', () => {
-      const expected = cold('a', {a: false});
-
-      expect(service.loggedIn$).toBeObservable(expected);
-    });
-
-    it('should have a value of false when initialized with an expired token', () => {
-      localStorage.setItem('id_token', 'someToken');
-      localStorage.setItem(
-        'expires_at',
-        moment(moment.now() - 3600)
-          .unix()
-          .toString()
+      expect(localStorage.getItem('id_token')).toEqual('someToken');
+      expect(localStorage.getItem('expires_at')).toEqual(
+          expiresAt.toString().valueOf()
       );
-      const expected = cold('a', {a: false});
-
-      expect(service.loggedIn$).toBeObservable(expected);
+      // verify the observable is set to the correct value after succesful login
+      expect(service.loggedIn$).toBeObservable(cold('b', {b: true}));
     });
 
-    it('should have a value of true when initialized with a token which is not expired', () => {
-      localStorage.setItem('id_token', 'someToken');
-      localStorage.setItem(
-        'expires_at',
-        moment(moment.now() + 3600)
-          .unix()
-          .toString()
-      );
-      const expected = cold('a', {a: true});
+    describe('isLoggedIn$', () => {
+      it('should have a value of false when initialized with no token in store', () => {
+        const expected = cold('a', {a: false});
 
-      expect(service.loggedIn$).toBeObservable(expected);
+        expect(service.loggedIn$).toBeObservable(expected);
+      });
+
+      it('should have a value of false when initialized with an expired token', () => {
+        localStorage.setItem('id_token', 'someToken');
+        localStorage.setItem(
+            'expires_at',
+            moment(moment.now() - 3600)
+                .unix()
+                .toString()
+        );
+        const expected = cold('a', {a: false});
+
+        expect(service.loggedIn$).toBeObservable(expected);
+      });
+
+      it('should have a value of true when initialized with a token which is not expired', () => {
+        localStorage.setItem('id_token', 'someToken');
+        localStorage.setItem(
+            'expires_at',
+            moment(moment.now() + 3600)
+                .unix()
+                .toString()
+        );
+        const expected = cold('a', {a: true});
+
+        expect(service.loggedIn$).toBeObservable(expected);
+      });
     });
   });
+
+  describe('logout', () => {
+    it('should clear localStorage on logout', () => {
+      localStorage.setItem('id_token', 'someToken');
+      localStorage.setItem('expires_at', moment(moment.now()).unix().toString());
+
+      service.logout();
+
+      expect(localStorage.getItem('id_token')).toBeNull();
+      expect(localStorage.getItem('expires_at')).toBeNull();
+    });
+  })
 });
